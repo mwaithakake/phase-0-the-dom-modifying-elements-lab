063711ba2a1bea194facd50cf4568653fb6b76ec
Martha <marthanyangatiwaithaka@gmail.com>
2024-04-02 12:10:37 +0300

    Updated file

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+ const element = document.querySelector(`main#main`)
+ element.remove(`main#main`)
+
+const newHeader = document.createElement(`H1`)
+
+newHeader.id = `victory`
+newHeader.innerHTML = "Waithaka is the champion"
